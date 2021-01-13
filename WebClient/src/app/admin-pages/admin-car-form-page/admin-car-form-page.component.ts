41e9b6d16cf637c5faa3016bd2321d3c1597df4a
whaplz <whaplz222@gmail.com>
2021-01-13 14:39:35 +0300

    testing fromGroup and validartor

--- WebClient/src/app/admin-pages/admin-car-form-page/admin-car-form-page.component.ts
+++ WebClient/src/app/admin-pages/admin-car-form-page/admin-car-form-page.component.ts
@@ -18,32 +18,38 @@ export class AdminCarFormPageComponent implements OnInit{
   }
 
   ngOnInit(): void {
-        // получение данных из роутера
-        this.activatedRoute.params.forEach((params: Params) => {
-          // если данные по id есть, но этого быть не может)
-          // то мы отправляем запрос на получение данных
-          if (params.id !== undefined) {
-            // получение даных для отображения
-            this.carService.getCarViewData(params.id).subscribe((data: any) => {
-              this.carViewData = data as CarViewData;
-              // создание класса для отображения и изменения формы
-              this.carForm = this.fb.group({
-                stateNumber: [ this.carViewData.stateNumber, [Validators.required]],
-                color: [ this.carViewData.color, [Validators.required]],
-                yearOfIssue: [ this.carViewData.yearOfIssue, [Validators.required]],
-                markTitle: [ this.carViewData.markTitle, [Validators.required]],
-                markModel: [ this.carViewData.markModel, [Validators.required]],
-                name: [ this.carViewData.name, [Validators.required]],
-                surname: [ this.carViewData.surname, [Validators.required]],
-                patronymic: [ this.carViewData.patronymic, [Validators.required]],
-                passport: [ this.carViewData.passport, [Validators.required]],
-              });
-            });
-          }
+    // получение данных из роутера
+    this.activatedRoute.params.forEach((params: Params) => {
+      // если данные по id есть, но этого быть не может)
+      // то мы отправляем запрос на получение данных
+      if (params.id !== undefined) {
+        // получение даных для отображения
+        this.carService.getCarViewData(params.id).subscribe((data: any) => {
+          this.carViewData = data as CarViewData;
+
+          // создание класса для отображения и изменения формы
+          this.carForm = this.fb.group({
+            stateNumber: [ this.carViewData.stateNumber, [Validators.required, Validators.minLength(8)]],
+            color: [ this.carViewData.color, [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+            // tslint:disable-next-line: max-line-length
+            yearOfIssue: [ this.carViewData.yearOfIssue, [Validators.required, Validators.min(1950), Validators.max(new Date().getFullYear())]],
+            markTitle: [ this.carViewData.markTitle, [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+            markModel: [ this.carViewData.markModel, [Validators.required, Validators.maxLength(20)]],
+            name: [ this.carViewData.name, [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+            surname: [ this.carViewData.surname, [Validators.required, Validators.minLength(4), Validators.maxLength(60)]],
+            patronymic: [ this.carViewData.patronymic, [Validators.required, Validators.minLength(4), Validators.maxLength(30)]],
+            passport: [ this.carViewData.passport, [Validators.required, Validators.minLength(8)]],
+          });
         });
+      }
+    });
   }
 
+  // отправка данных о форме
   submit(): void{
     console.log(this.carForm.value);
   }
+
+  get stateNumber(): string { return this.carViewData.stateNumber; }
+  
 }
